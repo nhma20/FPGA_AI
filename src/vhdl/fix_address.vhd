1831251a491111750b557f6a9f385eb1d8320b81
nhma20 <76950970+nhma20@users.noreply.github.com>
2021-12-09 10:40:34 +0100

    updated header comment

--- src/vhdl/fix_address.vhd
+++ src/vhdl/fix_address.vhd
@@ -1,40 +1,23 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: SDU, UAS, DIII
+-- Engineer: Nicolaj Malle
 -- 
 -- Create Date: 10/26/2021 12:50:35 PM
--- Design Name: 
--- Module Name: fix_address - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Project Name: FPGA_AI
+-- Target Devices: PYNQ-Z2
+-- Tool Versions: 2021.1
+-- Description: Aligns nn_inference IP module output address with BRAM
+-- input address
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity fix_address is
 
     GENERIC(
-       nn_address_output_width              : INTEGER := 7 -- numbers to average over
+       nn_address_output_width              : INTEGER := 7
        );
 
     Port ( addr_in : in STD_LOGIC_VECTOR (nn_address_output_width-1 downto 0);

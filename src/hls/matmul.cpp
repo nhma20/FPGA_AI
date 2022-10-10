121ffbd957936fbcd4adf1478f442bed980d5841
nhma20 <76950970+nhma20@users.noreply.github.com>
2022-10-10 15:43:27 +0200

    working fixed point implementation

--- src/hls/matmul.cpp
+++ src/hls/matmul.cpp
@@ -1,15 +1,29 @@
 #include "matmul.hpp"
 
 
+void float_to_fixed(float input[n_inputs], ap_fixed<32,24> output[n_inputs]) {
+	for(int i = 0; i < n_inputs; i++){
+		output[i] = input[i];
+	}
+}
+
+
+//void fixed_to_float(ap_fixed<32,24> input[n_inputs], float output[n_inputs]) {
+//	for(int i = 0; i < n_inputs; i++){
+//		output[i] = input[i];
+//	}
+//}
+
+
 /* Layer 1 matrix multiplication */
-void hwmm_layer1(float input[n_inputs], const float weights[n_inputs][n_layer1], float output[1][n_layer1]) {
+void hwmm_layer1(ap_fixed<32,24> input[n_inputs], const ap_fixed<32,24> weights[n_inputs][n_layer1], ap_fixed<32,24> output[1][n_layer1]) {
 
     col: for (int j = 0; j < n_layer1; ++j) {
-//#pragma HLS UNROLL
-      float sum = 0;
+//#pragma HLS UNROLL factor = 4
+      ap_fixed<32,24> sum = 0;
 
       prod: for (int k = 0; k < n_inputs; ++k){
-//#pragma HLS UNROLL
+#pragma HLS UNROLL factor=2
         sum += input[k] * weights[k][j];
       }
       output[0][j] = sum;
@@ -21,22 +35,18 @@ void hwmm_layer1(float input[n_inputs], const float weights[n_inputs][n_layer1],
 
 
 /* Layer 2 matrix multiplication */
-void hwmm_layer2(float input[1][n_layer1], const float weights[n_layer1][n_layer2], float output[1][n_layer2]) {
-
-  row: for (int i = 0; i < 1; ++i){
-//#pragma HLS UNROLL
+void hwmm_layer2(ap_fixed<32,24> input[1][n_layer1], const ap_fixed<32,24> weights[n_layer1][n_layer2], ap_fixed<32,24> output[1][n_layer2]) {
 
     col: for (int j = 0; j < n_layer2; ++j) {
 //#pragma HLS UNROLL
-      float sum = 0;
+      ap_fixed<32,24> sum = 0;
 
       prod: for (int k = 0; k < n_layer1; ++k){
 //#pragma HLS UNROLL
-        sum += input[i][k] * weights[k][j];
+        sum += input[0][k] * weights[k][j];
       }
-      output[i][j] = sum;
+      output[0][j] = sum;
     }
-  }
 
   return;
 }
@@ -44,29 +54,26 @@ void hwmm_layer2(float input[1][n_layer1], const float weights[n_layer1][n_layer
 
 
 /* Layer 3 matrix multiplication */
-void hwmm_layer3(float input[1][n_layer2], const float weights[n_layer2][n_layer3], float output[1][n_layer3]) {
-
-  row: for (int i = 0; i < 1; ++i){
-//#pragma HLS UNROLL
+void hwmm_layer3(ap_fixed<32,24> input[1][n_layer2], const ap_fixed<32,24> weights[n_layer2][n_layer3], ap_fixed<32,24> output[1][n_layer3]) {
 
     col: for (int j = 0; j < n_layer3; ++j) {
 //#pragma HLS UNROLL
-      float sum = 0;
+      ap_fixed<32,24> sum = 0;
 
       prod: for (int k = 0; k < n_layer2; ++k){
 //#pragma HLS UNROLL
-        sum += input[i][k] * weights[k][j];
+        sum += input[0][k] * weights[k][j];
       }
-      output[i][j] = sum;
+      output[0][j] = sum;
     }
-  }
+
   return;
 }
 
 
 
 /* ReLU layer 1 activation function */
-void hw_act_layer1(float input[1][n_layer1], float output[1][n_layer1]){
+void hw_act_layer1(ap_fixed<32,24> input[1][n_layer1], ap_fixed<32,24> output[1][n_layer1]){
 	loop1: for (int i = 0; i < n_layer1; i++){
 //#pragma HLS UNROLL
 		if (input[0][i] < 0.0)
@@ -81,7 +88,7 @@ void hw_act_layer1(float input[1][n_layer1], float output[1][n_layer1]){
 
 
 /* ReLU layer 2 activation function */
-void hw_act_layer2(float input[1][n_layer2], float output[1][n_layer2]){
+void hw_act_layer2(ap_fixed<32,24> input[1][n_layer2], ap_fixed<32,24> output[1][n_layer2]){
 	loop1: for (int i = 0; i < n_layer2; i++){
 //#pragma HLS UNROLL
 		if (input[0][i] < 0.0)
@@ -96,13 +103,13 @@ void hw_act_layer2(float input[1][n_layer2], float output[1][n_layer2]){
 
 
 /* Softmax layer 3 activation function */
-void hw_act_layer3(float input[1][n_layer3], int &pred){
-	int max_idx = -1;
-	float max_val = -999.9;
+void hw_act_layer3(ap_fixed<32,24> input[1][n_layer3], ap_fixed<32,24> &pred){
+	ap_fixed<32,24> max_idx = -1;
+	ap_fixed<32,24> max_val = -126;
 	loop1: for (int i = 0; i < n_layer3; i++){
 //#pragma HLS UNROLL
 		if (input[0][i] > max_val){
-			max_idx = i;
+			max_idx = (ap_fixed<32,24>)i;
 			max_val = input[0][i];
 		}
 	}
@@ -113,21 +120,33 @@ void hw_act_layer3(float input[1][n_layer3], int &pred){
 
 
 /* Connect NN Layers */
+//ap_fixed<32,24> nn_inference(float input_img[n_inputs]){
 int nn_inference(float input_img[n_inputs]){
+//#pragma HLS INTERFACE mode=s_axilite port=return
+//#pragma HLS INTERFACE mode=m_axi port=input_img
+
 //#pragma HLS ARRAY_PARTITION dim=1 type=complete variable=input_img
 
-	float temp_output[1][n_layer1] = {1};
-	float temp_output2[1][n_layer2] = {1};
-	float temp_output3[1][n_layer3] = {1};
-	int prediction = -1;
+	ap_fixed<32,24> fp_input_img[n_inputs] = {1.0};
+	float_to_fixed(input_img, fp_input_img);
 
-	hwmm_layer1(input_img, weights::layer1_weights, temp_output);
+	ap_fixed<32,24> temp_output[1][n_layer1] = {1};
+	ap_fixed<32,24> temp_output2[1][n_layer2] = {1};
+	ap_fixed<32,24> temp_output3[1][n_layer3] = {1};
+	ap_fixed<32,24> prediction = -1;
+	int prediction_int = -1;
+
+	hwmm_layer1(fp_input_img, weights::layer1_weights, temp_output);
 	hw_act_layer1(temp_output, temp_output);
 	hwmm_layer2(temp_output, weights::layer2_weights, temp_output2);
 	hw_act_layer2(temp_output2, temp_output2);
 	hwmm_layer3(temp_output2, weights::layer3_weights, temp_output3);
 	hw_act_layer3(temp_output3, prediction);
 
-	return prediction;
+	prediction_int = prediction;
+
+	return prediction_int;
 
 }
+
+
